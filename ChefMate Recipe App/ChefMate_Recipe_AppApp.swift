ab7383e1b58b2c411f3fdc739dec212b0231b90d
Ariel Bendahan <ariellielrey@hotmail.com>
2025-02-12 15:01:46 -0500

    Initial Commit

---
+++ ChefMate Recipe App/ChefMate_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefMate_Recipe_AppApp.swift
+//  ChefMate Recipe App
+//
+//  Created by Ariel on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct ChefMate_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefMate Recipe App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChefMate Recipe App
+//
+//  Created by Ariel on 2025-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
