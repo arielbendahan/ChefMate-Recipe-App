aa1d853e80281d356cc3ca2ef893efd7a8e2f65e
Ariel Bendahan <ariellielrey@hotmail.com>
2025-02-19 14:11:31 -0500

    Test 2

--- ChefMate Recipe App/ContentView.swift
+++ ChefMate Recipe App/ContentView.swift
@@ -14,8 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-            Text("On gang")
-            Text("TS PMO!")
         }
         .padding()
     }
