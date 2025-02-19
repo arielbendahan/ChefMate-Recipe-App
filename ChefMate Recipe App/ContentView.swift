5211b66c06382183fe9571258e0fad93057ffaaf
Mohammed Waseq Rahman <waseqrahman7@gmail.com>
2025-02-19 13:34:41 -0500

    ts pmo on gng

--- ChefMate Recipe App/ContentView.swift
+++ ChefMate Recipe App/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("On gang")
+            Text("TS PMO!")
         }
         .padding()
     }
