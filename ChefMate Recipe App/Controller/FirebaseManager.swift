871c763cc84b93fc24f2d1d733b87ebc63cb7f78
Ariel Bendahan <ariellielrey@hotmail.com>
2025-03-06 09:58:01 -0500

    Added proper error handling for when the user puts an incorrect email or password and deleted redudant file

--- ChefMate Recipe App/Controller/FirebaseManager.swift
+++ ChefMate Recipe App/Controller/FirebaseManager.swift
@@ -9,26 +9,6 @@ import Foundation
 import FirebaseCore
 import FirebaseAuth
 
-/*
- class FirebaseManager {
- static let shared = FirebaseManager()
- 
- private init() {}
- 
- // Login with Authentication
- func login(user: UserModel, completion: @escaping (Result<User, Error>) -> Void) {
- Auth.auth().signIn(withEmail: user.email, password: user.password) { authResult, error in
- if let error = error {
- completion(.failure(error))
- }
- else if let user = authResult?.user {
- completion(.success(user))
- }
- }
- }
- }
- */
-
 class AuthManager : ObservableObject {
     @Published var user: User?
     @Published var errorMessage: String?
@@ -42,12 +22,18 @@ class AuthManager : ObservableObject {
     func login(user: UserModel, completion: @escaping (Bool) -> Void) {
         Auth.auth().signIn(withEmail: user.email, password: user.password) {result, error in
             DispatchQueue.main.async {
-                if let error = error{
-                    self.errorMessage = error.localizedDescription
+                if let error = error as NSError?{
+                    switch error.code {
+                    case AuthErrorCode.invalidCredential.rawValue:
+                        self.errorMessage = "Incorrect email or password. Please try again"
+                    default:
+                        self.errorMessage = error.localizedDescription
+                    }
                     completion(false)
                 } else {
                     self.user = result?.user
                     self.isAuthenticated = true
+                    print("Login successful")
                     completion(true)
                 }
             }
