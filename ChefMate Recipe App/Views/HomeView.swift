850fb91f7811abcb3c2ff6a8eddd234ee90b88f3
Ariel Bendahan <ariellielrey@hotmail.com>
2025-03-16 00:16:31 -0400

    Added backup API key and commented out some things in the home screen. Remember, only use designer for API calls that use a few points

--- ChefMate Recipe App/Views/HomeView.swift
+++ ChefMate Recipe App/Views/HomeView.swift
@@ -9,59 +9,63 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.white)
-                    .frame(height: 100) // Adjust height as needed
-                    .shadow(color: .gray.opacity(0.5), radius: 5, x: 0, y: 3)
-                    .ignoresSafeArea()
-                
-
+        ScrollView{
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(height: 100) // Adjust height as needed
+                        .shadow(color: .gray.opacity(0.5), radius: 5, x: 0, y: 3)
+                        .ignoresSafeArea()
+                    
+                    
+                    HStack {
+                        Text("ChefMate")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.orange)
+                            .padding(.leading)
+                            .padding(.bottom, 50)
+                        Spacer()
+                    }
+                }
+                .frame(maxWidth: .infinity)
                 HStack {
-                    Text("ChefMate")
-                        .font(.title)
+                    Text("Featured Recipes")
+                        .font(.title2)
                         .bold()
-                        .foregroundColor(.orange)
                         .padding(.leading)
-                        .padding(.bottom, 50)
                     Spacer()
                 }
-            }
-            .frame(maxWidth: .infinity)
-            HStack {
-                Text("Featured Recipes")
-                    .font(.title2)
-                    .bold()
-                    .padding(.leading)
+                RecipeScrollView()
+                Divider()
+                    .frame(width: 300)
+                    .padding()
+                HStack {
+                    Text("Trending Recipes")
+                        .font(.title2)
+                        .bold()
+                        .padding(.leading)
+                    Spacer()
+                }
+                // DO NOT UNCOMMENT OR TURN OFF PREVIEWS BECAUSE THIS WILL DRAIN THE API LIMIT FOR THE DAY
+                //RecipeScrollView()
+                Divider()
+                    .frame(width: 300)
+                    .padding()
+                HStack {
+                    Text("Favourited Recipes")
+                        .font(.title2)
+                        .bold()
+                        .padding(.leading)
+                    Spacer()
+                }
+                // DO NOT UNCOMMENT OR TURN OFF PREVIEWS BECAUSE THIS WILL DRAIN THE API LIMIT FOR THE DAY
+                //RecipeScrollView()
+                
                 Spacer()
+                
             }
-            RecipeScrollView()
-            Divider()
-                .frame(width: 300)
-                .padding()
-            HStack {
-                Text("Trending Recipes")
-                    .font(.title2)
-                    .bold()
-                    .padding(.leading)
-                Spacer()
-            }
-            RecipeScrollView()
-            Divider()
-                .frame(width: 300)
-                .padding()
-            HStack {
-                Text("Favourited Recipes")
-                    .font(.title2)
-                    .bold()
-                    .padding(.leading)
-                Spacer()
-            }
-            RecipeScrollView()
-            
-            Spacer()
-
         }
     }
 }
